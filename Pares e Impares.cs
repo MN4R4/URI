2d9bb5ccd0a06e8da40021320a860f31430cb9fd
Marcos N4R4 <mknara@outlook.com>
2018-09-21 17:19:02 -0300

    C# - Estruturas e Bibliotecas

---
+++ Pares e Impares.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2808
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            ArrayList par = new ArrayList();
+            ArrayList impar = new ArrayList();
+
+            for (int i = 0; i < a; i++)
+            {
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (b % 2 == 0) par.Add(b);
+                else impar.Add(b);
+            }
+            par.Sort();
+            impar.Sort();
+
+            for (int i = 0; i < par.Count; i++)
+            {
+                Console.WriteLine(par[i]);
+            }
+
+            for (int j = impar.Count - 1; j >= 0; j--)
+            {
+                Console.WriteLine(impar[j]);
+            }
+
+            par.Clear(); impar.Clear();
+            Console.ReadLine();
+        }
+    }
+}
